816093a50217b9bcf332d4b40ab6d462b7573483
Jack <86924197+Z1nx-1@users.noreply.github.com>
2024-05-21 00:31:48 +0100

    Add files via upload
    Not functional but has fundamental concept

--- main.py
+++ main.py
@@ -2,30 +2,50 @@
 list = []
 
 
+def main():
+    try:
+        while True:
+            user_input = input("1) Add to list\n2) search list\n")
+            if user_input == '1':
+                add_to_list()
+            elif user_input == '2':
+                search_list()
+    except ValueError as e:
+        print(f"{e} has caused an error")
+
 
 def add_to_list():
-        user_input = input("What do you want to add? ")
-        list.append(user_input)
-        print(list)
-        return user_input
-
-def save_to_file(user_input):
-    with open('list.txt', 'a') as file:
-        confirm = input(f"Are you sure you want to add, {user_input}? Y/N: ")
+    while True:
+        user_input = '\n' + input("What do you want to add? ")
+        confirm = input(f"Is this correct? {user_input} \nY/N: ")
         if confirm == 'y':
-            file.write("\n")
-            file.write(user_input)
+            list.append(user_input)
+            save_to_file()
+
         elif confirm == 'n':
-            print("Passing")
-            pass
+            print("Please start again. ")
+            continue
+
+        else:
+            print("\n\n\nERROR, please enter a valid option: ")
+
+
+def save_to_file():
+    with open('list.txt', 'w') as file:
+        for content in list:
+            file.write(content)
+            print(content)
 
 
 def list_function():
     with open('list.txt', 'r') as file:
-        list.append(file.read())
+        list.append(file.read().split('\n'))
+        print(list)
 
+
+def search_list():
+    pass
 
 if __name__ == '__main__':
     list_function()
-    user_input = add_to_list()
-    save_to_file(user_input)
+    main()
