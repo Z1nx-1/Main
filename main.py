4301d45127746bf4f2ea22d67e3fc1fe4cc96aa0
Jack <86924197+Z1nx-1@users.noreply.github.com>
2024-05-21 01:57:57 +0100

    Add files via upload
    Added a search and remove function.
    Just need to idiot-proof it.

--- main.py
+++ main.py
@@ -1,50 +1,71 @@
 
-list = []
+def list_function():
+    global coll
+    with open('list.txt', 'r') as file:
+        coll = file.read().split('\n')
 
 
 def main():
     try:
         while True:
-            user_input = input("1) Add to list\n2) search list\n")
+            user_input = input("1)Add to list\n2)Search list\n3)Remove from list\n4)Save data to file\n5)Print list\n")
             if user_input == '1':
                 add_to_list()
             elif user_input == '2':
                 search_list()
+            elif user_input == '3':
+                remove_list()
+            elif user_input == '4':
+                save_to_file()
+            elif user_input == '5':
+                val = 0
+                print("\n\n------")
+                for content in coll:
+                    val += 1
+                    print(f"{val}) {content}")
+                print("------\n\n")
     except ValueError as e:
         print(f"{e} has caused an error")
 
 
 def add_to_list():
-    while True:
-        user_input = '\n' + input("What do you want to add? ")
-        confirm = input(f"Is this correct? {user_input} \nY/N: ")
-        if confirm == 'y':
-            list.append(user_input)
-            save_to_file()
+    user_input = '\n' + input("What do you want to add? ")
+    confirm = input(f"Is this correct? {user_input} \nY/N: ")
+    if confirm == 'y':
+        coll.append(user_input)
+        save_to_file()
 
-        elif confirm == 'n':
-            print("Please start again. ")
-            continue
+    elif confirm == 'n':
+        print("Please start again. ")
 
-        else:
-            print("\n\n\nERROR, please enter a valid option: ")
+    else:
+        print("\n\n\nERROR, please enter a valid option: ")
 
 
 def save_to_file():
     with open('list.txt', 'w') as file:
-        for content in list:
+        for content in coll:
+            file.write('\n')
             file.write(content)
             print(content)
 
 
-def list_function():
-    with open('list.txt', 'r') as file:
-        list.append(file.read().split('\n'))
-        print(list)
-
-
 def search_list():
-    pass
+    target = input("Enter name you want to search: ")
+    if target in coll:
+        print("That is in the file! ")
+
+    else:
+        print("That is not in the file! ")
+
+
+def remove_list():
+    target = input("Enter the name you want to remove: ")
+    coll.remove(target)
+    for content in coll:
+        print(content)
+    print(coll)
+
 
 if __name__ == '__main__':
     list_function()
