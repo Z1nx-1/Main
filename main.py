ece3855c4c8e24a64ae12543d00c361239a5476d
Jack <86924197+Z1nx-1@users.noreply.github.com>
2024-06-04 22:40:50 +0100

    Commit

--- main.py
+++ main.py
@@ -11,82 +11,90 @@ def main():
     try:
         coll = list_function()
         while True:
-            user_input = input("----------MAIN MENU----------\n1)Add to list\n2)Search list\n3)Remove from list\n4)Save changes\n5)Cancel changes/Refresh\n6)Print list\n-----------------------------\n")
-            if user_input == '1':
-                add_to_list(coll)
-            elif user_input == '2':
-                search_list(coll)
-            elif user_input == '3':
-                remove_list(coll)
-            elif user_input == '4':
-                save_to_file(coll)
-            elif user_input == '5':
-                main()  # by recalling the main() function the list 'coll' will get redefined with 'coll = list_function()' at the start of main().
-                for content in coll:
-                    print(content)
-            elif user_input == '6':
-                val = 0
-                print("\n\n------")
-                for content in coll:  # This for-loop will give each item in the list an ascending number.
-                    val += 1
-                    print(f"{val}){content}")
-                print("------\n\n")
-            else:
-                print("\nInvalid option, please try again:\n\n\n")
+            user_input = int(input("----------MAIN MENU----------\n1)Add to list\n2)Search list\n3)Remove from list\n4)Save changes\n5)Cancel changes/Refresh\n6)Print list\n-----------------------------\n"))
+            match user_input:
+                case 1:
+                    add_to_list(coll)
+                case 2:
+                    add_to_list(coll)
+                case 3:
+                    remove_list(coll)
+                case 4:
+                    save_to_file(coll)
+                case 5:
+                    main()  # by recalling the main() function the list 'coll' will get redefined with 'coll = list_function()' at the start of main().
+                case 6:
+                    for_print_function(coll)
+                    print("------\n\n")
+                case _:
+                    print("\nInvalid option, please try again:\n\n\n")
+
     except ValueError as e:
         print(f"{e} has caused an error")
 
 
 # This function will ask for user input that will be appended to the end of the 'coll' list.
 def add_to_list(coll):
-    try:
-        user_input = input("\nType '//' to go back\nWhat do you want to add?\n")
-        if user_input == '//':
-            print("\n")
-            return
-        confirm = input(f"\nIs this correct? {user_input} \nY/N: ")
-        if confirm.lower() == 'y':
-            coll.append(user_input)
-            print(f"\n\n{coll}\n\n")
-        elif confirm.lower() == 'n':
-            add_to_list(coll)
+    while True:
+        try:
+            for_print_function(coll)
+            user_input = input("\nType '//' to go back\nWhat do you want to add?\n")
+            if user_input == '//':
+                print("\n")
+                return
+            confirm = input(f"\nIs this correct? {user_input} \nY/N: ")
+            if confirm.lower() == 'y':
+                coll.append(user_input)
+                print(f"\n\n{coll}\n\n")
+            elif confirm.lower() == 'n':
+                add_to_list(coll)
 
-        else:
-            print("\n\n\nERROR, please enter a valid option: ")
-    except ValueError:
-        print("ERROR!")
+            else:
+                print("\n\n\nERROR, please enter a valid option: ")
+        except ValueError:
+            print("ERROR!")
 
 
 # This function gives the user the ability to search the list for a specific item.
 def search_list(coll):
-    try:
-        target = input("\nType '//' to go back\nEnter name you want to search: ")
-        if target == '//':
-            print("\n")
-            return
-        if target in coll:
-            print(f"{target} has been found in the file!")
-        else:
-            print("That is not in the file! ")
-    except ValueError:
-        print("ERROR!")
+    while True:
+        try:
+            target = input("\nType '//' to go back\nEnter name you want to search: ")
+            if target == '//':
+                print("\n")
+                return
+            if target in coll:
+                print(f"{target} has been found in the file!")
+            else:
+                print("That is not in the file! ")
+        except ValueError:
+            print("ERROR!")
 
 
 # This function, like the 'search_list' function will ask the user for an item name and then remove that item from the list.
 def remove_list(coll):
-    try:
-        for content in coll:
-            print(content)
-        target = input("\nType '//' to go back\nWhich item would you like to remove?\n")
-        if target == '//':
-            print(f"\n")
-            return
-        coll.remove(target)
-        print("\n\n\nUpdated list: ")
-        for content in coll:
-            print(content)
-    except ValueError:
-        print("ERROR! That item doesn't exist in the list! ")
+    val = 0
+    while True:
+        try:
+            if val == 0:  # The val variable is used to stop the initial unedited version of the list appearing after a removal of an item, this works due to the first loop doesn't increment val which means it can print the initial list.
+                for_print_function(coll)
+            target = input("\nType '//' to go back\nWhich item would you like to remove?\n")
+            if target == '//':
+                print(f"\n")
+                return
+
+            confirm = input(f"\nIs this correct? {target} \nY/N: ")
+            if confirm.lower() == 'y':
+                coll.append(target)
+                print(f"\n\n{coll}\n\n")
+            elif confirm.lower() == 'n':
+                add_to_list(coll)
+            coll.remove(target)
+            print("\n\n\nUpdated list: ")
+            for_print_function(coll)
+            val += 1
+        except ValueError:
+            print("ERROR! That item doesn't exist in the list! ")
 
 
 # This function is used when the user has confirmed and is ready to commit the changes made in the local list into the text file.
@@ -95,6 +103,14 @@ def save_to_file(coll):
         for content in coll:
             file.write(content + '\n')
             print(content)
+
+
+def for_print_function(coll):
+    val = 0
+    print("\n\n------\nCurrent contents:")
+    for content in coll:  # This for-loop will print the contents of the list, one by one and will give each item an ascending number.
+        val += 1
+        print(f"{val}){content}")
 
 
 if __name__ == '__main__':
