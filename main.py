f0b44773124bb2b849de69673cc359dc60203958
jack Wallace <jlwallace2006@gmail.com>
2024-07-15 09:07:35 +0100

    I have implemented a working index and name searching and removing feature, would be useful when there is mulitple items with the same name.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-
-# This function will be called then assign the contents of the text file 'list.txt' to a list called 'coll'(Collection).
+# ToDO ~ To implement index or name function without cluttering the code, ask the user first, pass that value into the function, depending on the value
+# ToDO ~ that the user typed, index or name would be called.
+# ToDo ~ This function will be called then assign the contents of the text file 'list.txt' to a list called 'coll'(Collection).
 def list_function():
     with open('list.txt', 'r') as file:
         coll = file.read().split()
@@ -58,18 +59,10 @@ def search_list(coll):
     while True:
         try:
             for_print_function(coll)
-            print("Enter the name of the item you would like to search: ")
-            lower_case_converted_list = lower_case_coll_function(coll)
-            target = user_function()
+            target = name_or_index_function(coll)
             if target is False:
                 break
-            match target.lower() in lower_case_converted_list:
-                case True:
-                    print(f"{target} has been found in the file!")
-                case False:
-                    print(f"{target} has not been found in the file!")
-                case _:
-                    print("ERROR")
+            print(f"\nItem index| {target}\n Item name| {coll[target]}")
         except ValueError:
             print("ERROR!")
         except Exception as e:
@@ -82,14 +75,21 @@ def remove_list(coll):
     while True:
         try:
             for_print_function(coll)
-            target = user_function_with_confirm()
+            target = name_or_index_function(coll)
             if target is False:
                 break
-            elif target in coll:
-                print(f"{target} found and has been removed!\n")
-                coll.remove(target)
-            else:
-                print(f"{target} has not been found, no changes have been made!")
+            confirm = input(f"\nType '//' to return to main menu:\nAre you sure you want to remove this item?\nItem index| {target}\n Item name| {coll[target]}\nY/N:\n>>> ")
+            if confirm == '//':
+                break
+            match confirm.lower():
+                case 'y':
+                    del coll[target]
+                    print("Item(s) have been removed! ")
+                case 'n':
+                    print("Changes have not been made! ")
+                    continue
+                case _:
+                    print("ERROR! ")
         except ValueError:
             print("ERROR! That item doesn't exist in the list! ")
 
@@ -167,7 +167,7 @@ def lower_case_coll_function(coll):
 # that the list coll which the whole program uses won't be affected, then a for loop is again used but will print each item of both unsorted and sorted alongside each other
 # simultaneously in a table. The table has a feature depending on the size of the item inside both lists will determine how many spaces are added, this ensures the walls
 # don't move and stay connected as column.
-def list_sorting(coll):  # ToDo ~ make the function modular in size, i.e increase amount of '=====' borders depending on the size.
+def list_sorting(coll):  # ToDo ~ make the function modular in size, E.g increase amount of '=====' borders depending on the size.
     temporary_list = []
     for x in coll:
         temporary_list.append(x)
@@ -176,6 +176,43 @@ def list_sorting(coll):  # ToDo ~ make the function modular in size, i.e increas
     for content1, content2 in zip(coll, temporary_list):
         print("|", content1, " "*(15-len(content1)), "|", content2, " "*(13-len(content2)), "|")
     print("|===================================|\n\n")
+
+
+# name_or_index_function is used when a function that uses user input and might need to use a keyword or index, mainly if there are multiple
+# items with the same name, hence the usage of index.
+# The function also utilises nested function as the functions within only work in this work flow.
+def name_or_index_function(coll):
+    try:
+        # This function will ask the user for an index of an item and then return the integer
+        def index_function():
+            for_print_function(coll)
+            input_target_index = int(input("Enter the item's index number:\n>>> "))
+            return input_target_index
+
+        # This function will ask the user for the name of an item and then will find the item in the list then
+        # will get the index position of the item by using its name.
+        def name_function():
+            for_print_function(coll)
+            input_target_name = input("Enter the item's name:\n>>> ")
+            for target in range(len(coll)):
+                if coll[target] == input_target_name:
+                    return target
+            return input_target_name
+
+        user_input = input("Type '//' to return to main menu:\nDo you want to use item index or name? \n1)Index\n2)Name\n>>> ")
+        if user_input == '//':  # Returns to the main menu
+            return False
+        if user_input == '1':
+            target_input = index_function()
+            return target_input
+        elif user_input == '2':
+            target_input = name_function()
+            return target_input
+        #  I could use return target_input here but for some reason pycharm gives me a warning doing so.
+    except ValueError:
+        print("Value Error!")
+    except Exception as e:
+        print(f"ERROR!\nA general error has occurred!\nMore info: \n{e}")
 
 
 if __name__ == '__main__':
